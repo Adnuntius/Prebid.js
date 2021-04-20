a940166fb68a968d2018b5c93403e25e1dfbedf7
Mikael Lundin <mikael@adnuntius.com>
2021-04-20 22:36:05 +0200

    RTD Providers added to adnuntius rtd provider. no requested with promises.

--- modules/adnuntiusRtdProvider.js
+++ modules/adnuntiusRtdProvider.js
@@ -1,73 +1,71 @@
-import { getGlobal } from '../src/prebidGlobal.js';
+// import { getGlobal } from '../src/prebidGlobal.js';
 import { submodule } from '../src/hook.js'
-import { logMessage } from '../src/utils.js'
+import { logError, logMessage } from '../src/utils.js'
 import { ajax } from '../src/ajax.js';
 
 const tzo = new Date().getTimezoneOffset();
-let SEGMENT_LIST = []
 const ADN_USER_ID = JSON.parse(window.localStorage.getItem('adn.data')).browserId
-const ADN_URL = 'https://data.adnuntius.com/usr?tzo=' + tzo + '&browserId=' + ADN_USER_ID + '&folderId=';
+
 function init(config, userConsent) {
   return true;
 }
 
 const providers = {
   adnuntius: {
-    url: 'https://data.adnuntius.com/usr?tzo=' + tzo + '&browserId=' + ADN_USER_ID + '&folderId=',
-    function: function (res) {
-      logMessage(res)
+    url: params => {
+      return 'https://data.adnuntius.com/usr?tzo=' + tzo + '&browserId=' + ADN_USER_ID + '&folderId=' + params.folderId
+    },
+    function: res => {
+      logMessage('ADN: adn-return', res)
       return res
     },
   },
   novatiq: {
-    url: 'https://novatiq.consumor.io/segments/v1/novademo/45340f6d-d9ee-4ee9-b785-36f3e30ff1599999',
-    function: function (res) {
-      logMessage(res)
+    url: params => 'https://novatiq.consumor.io/segments/v1/novademo/45340f6d-d9ee-4ee9-b785-36f3e30ff1599999',
+    function: res => {
+      logMessage('ADN: novatiq-return', res)
       return res
     },
   }
 }
 
+// Make sure that ajax has a function as callback
 function prepProvider(provider) {
   return new Promise((resolve, reject) => {
-    ajax(providers[provider].url, {
-      success: function (res) { resolve(providers[provider].function(res)) },
+    ajax(providers[provider.name].url(provider), {
+      success: function (res) { resolve(providers[provider.name].function(res)) },
       error: function (err) { reject(err) }
     })
   });
 }
 
 function alterBidRequests(reqBidsConfigObj, callback, config, userConsent) {
-  const pbjsG = getGlobal()
   const params = config.params
-  const affectedBidders = (params.bidders) ? Object.keys(params.bidders).filter(function (bidder) {
-    return params.bidders[bidder] == true
-  }) : []
-  const affectedProviders = (params.providers) ? Object.keys(params.providers).filter(function (provider) {
-    return params.providers[provider]
-  }) : []
-
-  logMessage('ADN: UID', pbjsG.getUserIds())
-  logMessage('ADN: CONF:', config)
-  logMessage('ADN: CONF: reqbidconf', reqBidsConfigObj)
-  logMessage('ADN: Providers:', affectedProviders)
-
-  Promise.all([prepProvider('novatiq')]).then((values) => {
-    logMessage('ADN: PROMISE', values);
-  });
-  ajax(ADN_URL + config.params.folderId, {
-    success: function (response, req) {
-      const segments = JSON.parse(response).segments
-      SEGMENT_LIST = [...SEGMENT_LIST, ...segments]
-      pbjsG.setBidderConfig({
-        bidders: affectedBidders,
-        config: {
-          segments: SEGMENT_LIST
-        }
-      });
-      callback();
+  // const pbjsG = getGlobal()
+  // const affectedBidders = (params.bidders) ? Object.keys(params.bidders).filter(function (bidder) {
+  //   return params.bidders[bidder] == true
+  // }) : []
+  const affectedProviders = (params.providers) ? Object.keys(params.providers).map(provider => {
+    return {
+      ...params.providers[provider],
+      name: provider
     }
-  });
+  }) : []
+  logMessage('ADN: providers', affectedProviders);
+
+  const providerRequests = affectedProviders.map(provider => prepProvider(provider))
+  logMessage('ADN: providerReq', providerRequests);
+  Promise.allSettled(providerRequests).then((values) => {
+    logMessage('ADN: PROMISE', values);
+    // SEGMENT_LIST = [...SEGMENT_LIST, ...segments]
+    // pbjsG.setBidderConfig({
+    //   bidders: affectedBidders,
+    //   config: {
+    //     segments: SEGMENT_LIST
+    //   }
+    // });
+    callback();
+  }).catch(err => logError('ADN: err', err));
 }
 
 /** @type {RtdSubmodule} */
